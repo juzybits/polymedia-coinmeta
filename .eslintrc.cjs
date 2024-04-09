7b86cc1f060220fcb79b4594330ab5fb8d5e99a1
juzybits <96890594+juzybits@users.noreply.github.com>
2024-04-09 20:16:13 +0000

    Revert "add prettier"
    This reverts commit 17423fc84414b35efcc6dae350e0aa420a745416.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,7 +17,6 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/strict-type-checked',
         'plugin:@typescript-eslint/stylistic-type-checked',
-        'plugin:prettier/recommended',
     ],
     plugins: [],
     rules: {
