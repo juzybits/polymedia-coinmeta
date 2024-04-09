17423fc84414b35efcc6dae350e0aa420a745416
juzybits <96890594+juzybits@users.noreply.github.com>
2024-04-09 19:39:20 +0000

    add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,7 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/strict-type-checked',
         'plugin:@typescript-eslint/stylistic-type-checked',
+        'plugin:prettier/recommended',
     ],
     plugins: [],
     rules: {
