91c0bd4f15b734e968c5e525111b8a24e2d00d1a
juzybits <96890594+juzybits@users.noreply.github.com>
2024-04-11 19:39:57 +0000

    make-prod.ts: resize raw images and convert to webp

---
+++ src/cli/src/make-prod.ts
@@ -0,0 +1,55 @@
+import { cpSync, mkdirSync, readdirSync } from "fs";
+import path from "path";
+import sharp from "sharp";
+import { CoinMeta } from "./types.js";
+import { getFilename, readJsonFile } from "./utils.js";
+
+/* Config */
+const INPUT_META_FILE = "./data/raw-meta.json";
+const OUTPUT_META_FILE = "./data/prod-meta.json";
+const OUTPUT_IMAGE_RAW_DIR = "./data/raw-img";
+const OUTPUT_IMAGE_PROD_DIR = "./data/prod-img";
+
+async function main()
+{
+    // ensure the output directory exists
+    mkdirSync(OUTPUT_IMAGE_PROD_DIR, { recursive: true });
+
+    const rawMetas = readJsonFile<CoinMeta[]>(INPUT_META_FILE);
+    const prodMetas: CoinMeta[] = [];
+    for (const meta of rawMetas) {
+        const filename = getFilename(meta.type)
+        console.log(filename);
+
+        // find the raw image
+        const rawImagePath = findRawImagePath(OUTPUT_IMAGE_RAW_DIR, filename);
+        if (!rawImagePath) {
+            console.error(`Raw image not found for: ${filename}`);
+            continue;
+        }
+
+        const ext = path.extname(rawImagePath);
+
+        // resize and convert the image to .webp
+        if (ext === '.svg') {
+            const prodImagePath = `${OUTPUT_IMAGE_PROD_DIR}/${filename}.svg`;
+            cpSync(rawImagePath, prodImagePath);
+        } else {
+            const prodImagePath = `${OUTPUT_IMAGE_PROD_DIR}/${filename}.webp`;
+            await sharp(rawImagePath)
+                .resize({ height: 256 })
+                .webp({ quality: 60 })
+                .toFile(prodImagePath);
+        }
+    }
+    // writeJsonFile(OUTPUT_META_FILE, rawMetas);
+}
+
+void main();
+
+// Function to find the raw image path based on the filename without extension
+function findRawImagePath(directory: string, filename: string): string | null {
+    const files = readdirSync(directory);
+    const file = files.find(f => path.basename(f, path.extname(f)) === filename);
+    return file ? path.join(directory, file) : null;
+}
