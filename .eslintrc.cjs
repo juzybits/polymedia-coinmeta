79bc31021843a8c7f8b850028d7d6abf9b20b705
juzybits <96890594+juzybits@users.noreply.github.com>
2024-04-13 15:52:22 +0000

    simplify .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,22 +6,35 @@ module.exports = {
     parserOptions: {
         ecmaVersion: "latest",
         sourceType: "module",
-        project: [
-            "./tsconfig.json",
-            "./src/cli/tsconfig.json",
-            "./src/sdk/tsconfig.json",
-            "./src/web/tsconfig.json",
-        ],
+        project: [ "./tsconfig.json" ],
         tsconfigRootDir: __dirname,
     },
-    plugins: [],
+    plugins: ["react-refresh"],
     extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/strict-type-checked",
         "plugin:@typescript-eslint/stylistic-type-checked",
+        "plugin:react-hooks/recommended",
+        "plugin:react/jsx-runtime",
+        "plugin:react/recommended",
     ],
     rules: {
+        '@typescript-eslint/no-confusing-void-expression': 'off',
+        '@typescript-eslint/use-unknown-in-catch-callback-variable': 'off',
         "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+        "@typescript-eslint/no-floating-promises": "off",
+        "@typescript-eslint/no-misused-promises": "off",
+        "@typescript-eslint/prefer-nullish-coalescing": [ "error", { "ignoreConditionalTests": true } ],
+        "@typescript-eslint/restrict-template-expressions": "off",
+        "react-hooks/exhaustive-deps": "off",
+        "react/no-unescaped-entities": "off",
+        "react/prop-types": "off",
+        "react/react-in-jsx-scope": "off",
+    },
+    settings: {
+        react: {
+            version: "detect"
+        }
     },
     overrides: [
         {
@@ -35,36 +48,8 @@ module.exports = {
             },
         },
         {
-            files: ["src/sdk/**/*"],
-            env: { node: false },
-            rules: {
-            },
-        },
-        {
             files: ["src/web/**/*"],
             env: { browser: true },
-            plugins: ["react-refresh"],
-            extends: [
-                "plugin:react-hooks/recommended",
-                "plugin:react/jsx-runtime",
-                "plugin:react/recommended",
-            ],
-            rules: {
-                "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-                "@typescript-eslint/no-floating-promises": "off",
-                "@typescript-eslint/no-misused-promises": "off",
-                "@typescript-eslint/prefer-nullish-coalescing": [ "error", { "ignoreConditionalTests": true } ],
-                "@typescript-eslint/restrict-template-expressions": "off",
-                "react-hooks/exhaustive-deps": "off",
-                "react/no-unescaped-entities": "off",
-                "react/prop-types": "off",
-                "react/react-in-jsx-scope": "off",
-            },
-            settings: {
-                react: {
-                    version: "detect"
-                }
-            },
         },
     ],
 };
