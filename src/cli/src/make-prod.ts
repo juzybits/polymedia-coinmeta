09d47ab3aa72ca162ce488737a442d65eef0a4da
juzybits <96890594+juzybits@users.noreply.github.com>
2024-04-14 13:02:52 +0000

    cli: make-prod to delete OUTPUT_WEB_IMAGE_DIR

--- src/cli/src/make-prod.ts
+++ src/cli/src/make-prod.ts
@@ -1,5 +1,5 @@
 import { CoinMeta } from "@polymedia/coinmeta";
-import { cpSync } from "fs";
+import { cpSync, mkdirSync, rmSync } from "fs";
 import path from "path";
 import sharp from "sharp";
 import { findImagePath, getFilename, readJsonFile, writeJsonFile } from "./utils.js";
@@ -19,10 +19,14 @@ const INPUT_RAW_META_FILE = "../../data/raw-meta.json";
 const INPUT_RAW_IMAGE_DIR = "../../data/raw-img";
 const OUTPUT_SDK_META_FILE = "../sdk/src/data.json";
 const OUTPUT_WEB_META_FILE = "../web/public/api/data.json";
-const OUTPUT_WEB_IMAGE_DIR = "../web/public/img";
+const OUTPUT_WEB_IMAGE_DIR = "../web/public/img"; // careful, gets deleted
 
 async function main()
 {
+    // delete and re-create the output directory
+    rmSync(OUTPUT_WEB_IMAGE_DIR, { recursive: true });
+    mkdirSync(OUTPUT_WEB_IMAGE_DIR);
+
     const rawMetas = readJsonFile<CoinMeta[]>(INPUT_RAW_META_FILE);
     const prodMetas: CoinMeta[] = [];
     for (const meta of rawMetas) {
