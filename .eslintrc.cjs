fde23305c8d0c10451c0b3afe56064afe850ab3f
juzybits <96890594+juzybits@users.noreply.github.com>
2024-04-15 17:00:33 +0000

    eslint: add @stylistic

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,21 +9,23 @@ module.exports = {
         project: [ "./tsconfig.json" ],
         tsconfigRootDir: __dirname,
     },
-    plugins: [],
+    plugins: [
+        "@stylistic"
+    ],
     extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/strict-type-checked",
         "plugin:@typescript-eslint/stylistic-type-checked",
     ],
     rules: {
-        '@typescript-eslint/no-confusing-void-expression': 'off',
-        '@typescript-eslint/use-unknown-in-catch-callback-variable': 'off',
+        "@stylistic/quotes": [ "error", "double", { "avoidEscape": true } ],
         "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+        "@typescript-eslint/no-confusing-void-expression": "off",
         "@typescript-eslint/no-floating-promises": "off",
         "@typescript-eslint/no-misused-promises": "off",
         "@typescript-eslint/prefer-nullish-coalescing": [ "error", { "ignoreConditionalTests": true } ],
         "@typescript-eslint/restrict-template-expressions": "off",
-        "quotes": [ "error", "double", { "avoidEscape": true } ],
+        "@typescript-eslint/use-unknown-in-catch-callback-variable": "off",
     },
     overrides: [
         {
