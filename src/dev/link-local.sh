f070eeb29d85d5f803930e7097c2f54a3cc1f452
juzybits <96890594+juzybits@users.noreply.github.com>
2024-12-05 13:46:26 -0100

    dev: add link-local.sh

---
+++ src/dev/link-local.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -o nounset      # Treat unset variables as an error when substituting
+set -o errexit      # Exit immediately if any command returns a non-zero status
+set -o pipefail     # Prevent errors in a pipeline from being masked
+set -o xtrace       # Print each command to the terminal before execution
+
+PATH_SUITCASE=$HOME/data/code/polymedia-suitcase
+PATH_LOCAL=$HOME/data/code/polymedia-coinmeta
+
+cd $PATH_SUITCASE
+pnpm build
+
+cd $PATH_LOCAL/src/sdk
+pnpm link $PATH_SUITCASE/src/node
+
+cd $PATH_LOCAL/src/web
+pnpm link $PATH_SUITCASE/src/core
