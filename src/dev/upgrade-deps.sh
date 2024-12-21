ea65b80c5d2cb372e756aa5ec8fbffd0c87f87bd
juzybits <96890594+juzybits@users.noreply.github.com>
2024-12-21 13:15:08 -0100

    dev: add upgrade-deps.sh

---
+++ src/dev/upgrade-deps.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -o nounset      # Treat unset variables as an error when substituting
+set -o errexit      # Exit immediately if any command returns a non-zero status
+set -o pipefail     # Prevent errors in a pipeline from being masked
+set -o xtrace       # Print each command to the terminal before execution
+
+PATH_LOCAL=$HOME/data/code/polymedia-coinmeta
+REACT_VERSION="^18.0.0"
+
+cd $PATH_LOCAL
+pnpm upgrade --latest --recursive
+pnpm upgrade --latest -w
+
+cd $PATH_LOCAL/src/react
+pnpm add -D @types/react@$REACT_VERSION
+
+cd $PATH_LOCAL/src/web
+pnpm add react@$REACT_VERSION react-dom@$REACT_VERSION
+pnpm add -D @types/react@$REACT_VERSION @types/react-dom@$REACT_VERSION
