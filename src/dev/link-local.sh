43bd974e74f09954ed58bb110bd86479781b857d
juzybits <96890594+juzybits@users.noreply.github.com>
2025-01-01 11:57:57 -0100

    all: upgrade

--- src/dev/link-local.sh
+++ src/dev/link-local.sh
@@ -5,14 +5,14 @@ set -o errexit      # Exit immediately if any command returns a non-zero status
 set -o pipefail     # Prevent errors in a pipeline from being masked
 set -o xtrace       # Print each command to the terminal before execution
 
+PATH_THIS=$HOME/data/code/polymedia-coinmeta
 PATH_SUITCASE=$HOME/data/code/polymedia-suitcase
-PATH_LOCAL=$HOME/data/code/polymedia-coinmeta
 
 cd $PATH_SUITCASE
 pnpm build
 
-cd $PATH_LOCAL/src/cli
+cd $PATH_THIS/src/cli
 pnpm link $PATH_SUITCASE/src/node
 
-cd $PATH_LOCAL/src/web
+cd $PATH_THIS/src/web
 pnpm link $PATH_SUITCASE/src/react
--- src/dev/upgrade-deps.sh
+++ src/dev/upgrade-deps.sh
@@ -5,20 +5,20 @@ set -o errexit      # Exit immediately if any command returns a non-zero status
 set -o pipefail     # Prevent errors in a pipeline from being masked
 set -o xtrace       # Print each command to the terminal before execution
 
-PATH_LOCAL=$HOME/data/code/polymedia-coinmeta
+PATH_THIS=$HOME/data/code/polymedia-coinmeta
 REACT_VERSION="^18.0.0"
 
-cd $PATH_LOCAL
+cd $PATH_THIS
 pnpm up --latest --recursive
 pnpm up --latest -w
 
-cd $PATH_LOCAL/src/react
+cd $PATH_THIS/src/react
 pnpm add -D @types/react@$REACT_VERSION
 
-cd $PATH_LOCAL/src/web
+cd $PATH_THIS/src/web
 pnpm add react@$REACT_VERSION react-dom@$REACT_VERSION
 pnpm add -D @types/react@$REACT_VERSION @types/react-dom@$REACT_VERSION
 
-cd $PATH_LOCAL
+cd $PATH_THIS
 pnpm up --recursive
 pnpm up -w
