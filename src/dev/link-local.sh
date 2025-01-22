1f14c7e3f0e62fd4cf8cb153bd91b07a2b426b8d
juzybits <96890594+juzybits@users.noreply.github.com>
2025-01-22 18:18:34 -0100

    js: upgrade

--- src/dev/link-local.sh
+++ src/dev/link-local.sh
@@ -5,14 +5,16 @@ set -o errexit      # Exit immediately if any command returns a non-zero status
 set -o pipefail     # Prevent errors in a pipeline from being masked
 set -o xtrace       # Print each command to the terminal before execution
 
-PATH_THIS=$HOME/data/code/polymedia-coinmeta
+SCRIPT_DIR="$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )"
+PATH_PROJECT="$( cd "$SCRIPT_DIR/../.." && pwd )"
+
 PATH_SUITCASE=$HOME/data/code/polymedia-suitcase
 
 cd $PATH_SUITCASE
 pnpm build
 
-cd $PATH_THIS/src/cli
+cd $PATH_PROJECT/src/cli
 pnpm link $PATH_SUITCASE/src/node
 
-cd $PATH_THIS/src/web
+cd $PATH_PROJECT/src/web
 pnpm link $PATH_SUITCASE/src/react
